d9b23eea294731069dcc1aa11180e8048c17b6b0
jburgaya <judit.burgaya@gmail.com>
2024-07-11 15:41:04 +0200

    test

--- workflow/scripts/blast_output.py
+++ workflow/scripts/blast_output.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+ #!/usr/bin/env python
 
 import os
 import argparse
@@ -57,23 +57,23 @@ if __name__ == "__main__":
         # update categ_summary based on filtered results
         for v_category in vf["VAGs_category"].unique():
             if v_category in df_filtered["VAGs_category"].values:
-                categ_summary.loc[file, v_category] = 1
+                categ_summary.loc[os.path.splitext(file)[0], v_category] = 1
             else:
-                categ_summary.loc[file, v_category] = 0
+                categ_summary.loc[os.path.splitext(file)[0], v_category] = 0
 
         # update genes_summary based on filtered results
         for v_gene in vf["Gene"].unique():
             if v_gene in df_filtered["gene"].values:
-                gene_summary.loc[file, v_gene] = 1
+                gene_summary.loc[os.path.splitext(file)[0], v_gene] = 1
             else:
-                gene_summary.loc[file, v_gene] = 0
+                gene_summary.loc[os.path.splitext(file)[0], v_gene] = 0
 
         # update genes_summary based on filtered results
         for v_locustag in vf["Locus Tag"].unique():
             if v_locustag in df_filtered["qseqid"].values:
-                locustag_summary.loc[file, v_locustag] = 1
+                locustag_summary.loc[os.path.splitext(file)[0], v_locustag] = 1
             else:
-                locustag_summary.loc[file, v_locustag] = 0
+                locustag_summary.loc[os.path.splitext(file)[0], v_locustag] = 0
 
     # save summaries
     categ_summary_file = os.path.join(options.blastout, "summary_categ.tsv")
@@ -82,15 +82,3 @@ if __name__ == "__main__":
     gene_summary.to_csv(gene_summary_file, sep="\t", index=True, header=True)
     locustag_summary_file = os.path.join(options.blastout, "summary_locustag.tsv")
     locustag_summary.to_csv(locustag_summary_file, sep="\t", index=True, header=True)
-
-
-
-
-
-
-
-
-
-
-
-
